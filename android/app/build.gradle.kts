a7381e906f1a1d84e8528cb865b466b8f6b6b106
kimhy <kimhyyefla@gmail.com>
2025-05-22 15:18:59 +0900

    연동중

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,6 @@ android {
     ndkVersion = "27.0.12077973"
     namespace = "com.example.my_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
