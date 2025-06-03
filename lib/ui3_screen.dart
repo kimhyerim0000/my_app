6630cc87b0b2da7d760864e0cc5874a036c6ad52
kimhy <kimhyyefla@gmail.com>
2025-06-03 12:27:51 +0900

    ui3 완성

--- lib/ui3_screen.dart
+++ lib/ui3_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_database/firebase_database.dart';
 class UI3Screen extends StatefulWidget {
   const UI3Screen({super.key});
 
@@ -10,13 +10,25 @@ class UI3Screen extends StatefulWidget {
 class _UI3ScreenState extends State<UI3Screen> {
   double tempValue = 25.0;
   double humidityValue = 55.0;
-  bool pushEnabled = false;
+  bool autoEnabled = false;
+  bool ledEnabled = false;
+  bool fanEnabled = false;
+  // 라디오버튼의 데이터베이스 상태 적용
+  @override
+  void initState() {
+    super.initState();
+    loadSettingsFromFirebase(); // ✅ 설정값 Firebase에서 불러오기
+  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final scaleW = size.width / 200;
     final scaleH = size.height / 300;
+
 
     return Scaffold(
       backgroundColor: const Color(0xFFE0E0E0),
@@ -43,8 +55,8 @@ class _UI3ScreenState extends State<UI3Screen> {
                 child: GestureDetector(
                   onTap: () {
                     Navigator.pop(context, {
-                      'temp': tempValue,
-                      'humidity': humidityValue,
+                      // 'temp': tempValue,
+                      // 'humidity': humidityValue,
                     });
                   },
                   child: Icon(
@@ -71,11 +83,13 @@ class _UI3ScreenState extends State<UI3Screen> {
                             onIncrease: () {
                               setState(() {
                                 tempValue += 1;
+                                saveTemperatureAndHumidity(tempValue, humidityValue);
                               });
                             },
                             onDecrease: () {
                               setState(() {
                                 tempValue -= 1;
+                                saveTemperatureAndHumidity(tempValue, humidityValue);
                               });
                             },
                             scaleW: scaleW,
@@ -87,11 +101,13 @@ class _UI3ScreenState extends State<UI3Screen> {
                             onIncrease: () {
                               setState(() {
                                 humidityValue += 1;
+                                saveTemperatureAndHumidity(tempValue, humidityValue);
                               });
                             },
                             onDecrease: () {
                               setState(() {
                                 humidityValue -= 1;
+                                saveTemperatureAndHumidity(tempValue, humidityValue);
                               });
                             },
                             scaleW: scaleW,
@@ -99,9 +115,10 @@ class _UI3ScreenState extends State<UI3Screen> {
                           ),
                         ],
                       ),
+                      //auto
                       SizedBox(height: 30 * scaleH),
                       Text(
-                        "푸시 알림",
+                        "AUTO",
                         style: TextStyle(
                           fontSize: 5 * scaleW,
                           color: Colors.black,
@@ -114,10 +131,79 @@ class _UI3ScreenState extends State<UI3Screen> {
                           Text("off", style: TextStyle(fontSize: 4 * scaleW)),
                           SizedBox(width: 4 * scaleW),
                           Switch(
-                            value: pushEnabled,
+                            value: autoEnabled,
                             onChanged: (val) {
                               setState(() {
-                                pushEnabled = val;
+                                autoEnabled = val;
+                                saveSetting("AUTO", val);
+                              });
+                            },
+                            activeColor: Colors.white,
+                            activeTrackColor: Colors.grey,
+                            inactiveThumbColor: Colors.white,
+                            inactiveTrackColor: Colors.grey,
+                            materialTapTargetSize:
+                            MaterialTapTargetSize.shrinkWrap,
+                          ),
+                          SizedBox(width: 4 * scaleW),
+                          Text("on", style: TextStyle(fontSize: 4 * scaleW)),
+                        ],
+                      ),
+                      //led
+                      SizedBox(height: 30 * scaleH),
+                      Text(
+                        "LED",
+                        style: TextStyle(
+                          fontSize: 5 * scaleW,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 4 * scaleH),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("off", style: TextStyle(fontSize: 4 * scaleW)),
+                          SizedBox(width: 4 * scaleW),
+                          Switch(
+                            value: ledEnabled,
+                            onChanged: (val) {
+                              setState(() {
+                                ledEnabled = val;
+                                saveSetting("LED", val);
+                              });
+                            },
+                            activeColor: Colors.white,
+                            activeTrackColor: Colors.grey,
+                            inactiveThumbColor: Colors.white,
+                            inactiveTrackColor: Colors.grey,
+                            materialTapTargetSize:
+                            MaterialTapTargetSize.shrinkWrap,
+                          ),
+                          SizedBox(width: 4 * scaleW),
+                          Text("on", style: TextStyle(fontSize: 4 * scaleW)),
+                        ],
+                      ),
+                      //fan
+                      SizedBox(height: 30 * scaleH),
+                      Text(
+                        "FAN",
+                        style: TextStyle(
+                          fontSize: 5 * scaleW,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 4 * scaleH),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("off", style: TextStyle(fontSize: 4 * scaleW)),
+                          SizedBox(width: 4 * scaleW),
+                          Switch(
+                            value: fanEnabled,
+                            onChanged: (val) {
+                              setState(() {
+                                fanEnabled = val;
+                                saveSetting("FAN", val);
                               });
                             },
                             activeColor: Colors.white,
@@ -140,6 +226,43 @@ class _UI3ScreenState extends State<UI3Screen> {
         ),
       ),
     );
+  }
+  // setting/$key에 저장됨.
+  void saveSetting(String key, bool value) async {
+    final dbRef = FirebaseDatabase.instance.ref();
+    try {
+      await dbRef.child("settings/$key").set(value ? 1 : 0);
+      print('✅ settings/$key = ${value ? 1 : 0} 저장됨');
+    } catch (e) {
+      print('❌ settings/$key 저장 실패: $e');
+    }
+  }
+
+  // 설정한 버튼값 저장하기
+  void loadSettingsFromFirebase() async {
+    final dbRef = FirebaseDatabase.instance.ref();
+    final snapshot = await dbRef.child("settings").get();
+
+    if (snapshot.exists) {
+      final data = snapshot.value as Map<dynamic, dynamic>;
+      setState(() {
+        autoEnabled = data['AUTO'] ?? false;
+        ledEnabled = data['LED'] ?? false;
+        fanEnabled = data['FAN'] ?? false;
+      });
+      print("✅ Firebase에서 설정값 불러오기 성공: $data");
+    } else {
+      print("⚠️ Firebase 연결됨, 하지만 데이터 없음");
+    }
+  }
+  //온습도 값 저장
+  void saveTemperatureAndHumidity(double temp, double humidity) async {
+    final dbRef = FirebaseDatabase.instance.ref();
+    await dbRef.child("settings").update({
+      'TEMPERATURE': temp,
+      'HUMIDITY': humidity,
+    });
+    print('✅ 온습도 저장됨: settings/TEMPERATURE=$temp, settings/HUMIDITY=$humidity');
   }
 
   Widget _settingBox({
